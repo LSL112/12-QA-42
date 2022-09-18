617ad00b3702dcc8d5ea4d42ec73d67dbad05ad0
Сергей Лешуков <lsl775319@gmail.com>
2022-09-18 20:43:11 +0300

    First commit

--- src/main/java/ru/netology/afisha/Afisha.java
+++ src/main/java/ru/netology/afisha/Afisha.java
@@ -3,6 +3,46 @@ package ru.netology.afisha;
 
 public class Afisha {
 
+    private String[] films = new String[0];
+    private int limit;
 
-    
+
+    public Afisha() {
+        limit = 10;
+    }
+
+    public Afisha(int limit) {
+        this.limit = limit;
+    }
+
+
+    public void add(String film) {
+        String[] result = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            result[i] = films[i];
+        }
+        result[result.length - 1] = film;
+        films = result;
+    }
+
+
+    public String[] findAll() {
+        return films;
+    }
+
+
+    public String[] findLast() {
+        int length;
+        if (films.length < limit) {
+            length = films.length;
+        } else {
+            length = limit;
+        }
+
+        String[] result = new String[length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = films[films.length - 1 - i];
+        }
+        return result;
+    }
 }
--- src/test/java/ru/netology/afisha/AfishaTest.java
+++ src/test/java/ru/netology/afisha/AfishaTest.java
@@ -1,4 +1,89 @@
 package ru.netology.afisha;
 
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+
 public class AfishaTest {
+
+    @Test
+    public void testFindAll1() {
+        Afisha afisha = new Afisha();
+
+        afisha.add("Film 1");
+        afisha.add("Film 2");
+        afisha.add("Film 3");
+        afisha.add("Film 4");
+        afisha.add("Film 5");
+        afisha.add("Film 6");
+        afisha.add("Film 7");
+        afisha.add("Film 8");
+        afisha.add("Film 9");
+        afisha.add("Film 10");
+        afisha.add("Film 11");
+
+        String[] actual = afisha.findAll();
+        String[] expected = {"Film 1", "Film 2", "Film 3", "Film 4", "Film 5", "Film 6", "Film 7", "Film 8", "Film 9", "Film 10", "Film 11"};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testFindAll2() {
+        Afisha afisha = new Afisha();
+
+        afisha.add("Film 1");
+        afisha.add("Film 2");
+        afisha.add("Film 3");
+        afisha.add("Film 4");
+        afisha.add("Film 5");
+
+        String[] actual = afisha.findAll();
+        String[] expected = {"Film 1", "Film 2", "Film 3", "Film 4", "Film 5"};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void findLast1() {
+        Afisha afisha = new Afisha();
+
+        afisha.add("Film 1");
+        afisha.add("Film 2");
+        afisha.add("Film 3");
+        afisha.add("Film 4");
+        afisha.add("Film 5");
+        afisha.add("Film 6");
+        afisha.add("Film 7");
+        afisha.add("Film 8");
+        afisha.add("Film 9");
+        afisha.add("Film 10");
+        afisha.add("Film 11");
+
+        String[] actual = afisha.findLast();
+        String[] expected = {"Film 11", "Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2"};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void findLast2() {
+        Afisha afisha = new Afisha();
+
+        afisha.add("Film 1");
+        afisha.add("Film 2");
+        afisha.add("Film 3");
+        afisha.add("Film 4");
+        afisha.add("Film 5");
+
+        String[] actual = afisha.findLast();
+        String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
 }
